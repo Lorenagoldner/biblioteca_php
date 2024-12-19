8439502db15e7d2805daadee301c1d1f0ed7175b
Lorena <lologoldner@gmail.com>
2024-12-19 18:05:44 +0000

    Adicionada validacao de utilizador
    Adicionado o controle de usuario para validar role de acesso as paginas

--- resources/views/autores/create.blade.php
+++ resources/views/autores/create.blade.php
@@ -1,33 +1,38 @@
 <!-- resources/views/autores/create.blade.php -->
-
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4">
+@auth
+    @if (Auth::user()->role === 'admin')
+        <div class="container mt-4">
 
-    <h1>Adicionar Novo Autor</h1>
+            <h1>Adicionar Novo Autor</h1>
 
-    <form action="{{ route('autores.store') }}" method="POST">
-        @csrf
-        <div class="mb-3">
-            <label for="primeiro_nome" class="form-label">Primeiro Nome</label>
-            <input type="text" name="primeiro_nome" id="primeiro_nome" class="form-control" required>
+            <form action="{{ route('autores.store') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="primeiro_nome" class="form-label">Primeiro Nome</label>
+                    <input type="text" name="primeiro_nome" id="primeiro_nome" class="form-control" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="ultimo_nome" class="form-label">Último Nome</label>
+                    <input type="text" name="ultimo_nome" id="ultimo_nome" class="form-control" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="pais" class="form-label">País</label>
+                    <input type="text" name="pais" id="pais" class="form-control" required>
+                </div>
+
+                <button type="submit" class="btn btn-success">Adicionar Autor</button>
+            </form>
+
         </div>
-
-        <div class="mb-3">
-            <label for="ultimo_nome" class="form-label">Último Nome</label>
-            <input type="text" name="ultimo_nome" id="ultimo_nome" class="form-control" required>
-        </div>
-
-        <div class="mb-3">
-            <label for="pais" class="form-label">País</label>
-            <input type="text" name="pais" id="pais" class="form-control" required>
-        </div>
-
-
-        <button type="submit" class="btn btn-success">Adicionar Autor</button>
-    </form>
-
-</div>
+    @else
+        <p>Utilizador sem permissão para acessar a página</p>
+    @endif
+@else
+    <p>Favor realizar login para acessar o conteudo.</p>
+@endauth
 @endsection
-
--- resources/views/livros/create.blade.php
+++ resources/views/livros/create.blade.php
@@ -1,36 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4">
-    <h1 class="mb-4">Adicionar Novo Livro</h1>
+@auth
+    @if (Auth::user()->role === 'admin')
+        <div class="container mt-4">
+            <h1 class="mb-4">Adicionar Novo Livro</h1>
 
-    <form action="{{ route('livros.store') }}" method="POST">
-    @csrf
+            <form action="{{ route('livros.store') }}" method="POST">
+                @csrf
 
-    <label>Título:</label>
-    <input type="text" name="titulo" class="form-control" required>
+                <label>Título:</label>
+                <input type="text" name="titulo" class="form-control" required>
 
-    <label>Autor:</label>
-    <select name="autor_id" class="form-control" required>
-        @foreach ($autores as $autor)
-            <option value="{{ $autor->id }}">{{ $autor->primeiro_nome }} {{ $autor->ultimo_nome }}</option>
-        @endforeach
-    </select>
+                <label>Autor:</label>
+                <select name="autor_id" class="form-control" required>
+                    @foreach ($autores as $autor)
+                        <option value="{{ $autor->id }}">{{ $autor->primeiro_nome }} {{ $autor->ultimo_nome }}</option>
+                    @endforeach
+                </select>
 
-    <label>Gênero:</label>
-    <input type="text" name="genero" class="form-control" required>
+                <label>Gênero:</label>
+                <input type="text" name="genero" class="form-control" required>
 
-    <label>Língua:</label>
-    <input type="text" name="lingua" class="form-control" required>
+                <label>Língua:</label>
+                <input type="text" name="lingua" class="form-control" required>
 
-    <label>ISBN:</label>
-    <input type="text" name="isbn" class="form-control" required>
+                <label>ISBN:</label>
+                <input type="text" name="isbn" class="form-control" required>
 
-    <label>Ano de Publicação:</label>
-    <input type="number" name="publicacao_ano" class="form-control" required>
+                <label>Ano de Publicação:</label>
+                <input type="number" name="publicacao_ano" class="form-control" required>
 
-    <button type="submit" class="btn btn-success mt-3">Salvar</button>
-
-    </form>
-</div>
+                <button type="submit" class="btn btn-success mt-3">Salvar</button>
+            </form>
+        </div>
+    @else
+        <p>Utilizador sem permissão para acessar a página</p>
+    @endif
+@else
+    <p>Favor realizar login para acessar o conteudo.</p>
+@endauth
 @endsection
